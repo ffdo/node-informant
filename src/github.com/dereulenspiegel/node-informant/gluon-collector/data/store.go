ee23abff038f654a93bf811ff3bc3b572bde785a
Till Klocke <till.klocke@gmail.com>
2015-09-29 14:50:37 +0200

    collectors are now in their own file

--- src/github.com/dereulenspiegel/node-informant/gluon-collector/data/store.go
+++ src/github.com/dereulenspiegel/node-informant/gluon-collector/data/store.go
@@ -97,6 +97,10 @@ func (s *SimpleInMemoryStore) updateNodeStatusInfo(response ParsedResponse) {
 	s.statusInfo[response.NodeId()] = info
 }
 
+func (s *SimpleInMemoryStore) GetGraphData() {
+
+}
+
 func (s *SimpleInMemoryStore) GetNodeStatusInfo(nodeId string) (status NodeStatusInfo, err error) {
 	stat, exists := s.statusInfo[nodeId]
 	if !exists {
@@ -235,82 +239,4 @@ func (s *SimpleInMemoryStore) UpdateNodesJson() {
 			"path":  s.nodesJsonPath,
 		}).Error("Error writing nodes.json")
 	}
-}
-
-type GatewayCollector struct {
-	Store *SimpleInMemoryStore
-}
-
-func (g *GatewayCollector) Process(in chan ParsedResponse) chan ParsedResponse {
-	out := make(chan ParsedResponse)
-	go func() {
-		for response := range in {
-			if response.Type() == "statistics" {
-				statistics := response.ParsedData().(StatisticsStruct)
-				gateway := statistics.Gateway
-				if gateway != "" {
-					_, exists := g.Store.gatewayList[response.NodeId()]
-					if !exists {
-						g.Store.gatewayList[response.NodeId()] = true
-					}
-				}
-			}
-			out <- response
-		}
-	}()
-	return out
-}
-
-type NodeinfoCollector struct {
-	Store *SimpleInMemoryStore
-}
-
-func (n *NodeinfoCollector) Process(in chan ParsedResponse) chan ParsedResponse {
-	out := make(chan ParsedResponse)
-	go func() {
-		for response := range in {
-			if response.Type() == "nodeinfo" {
-				nodeinfo := response.ParsedData().(NodeInfo)
-				n.Store.nodeinfos[nodeinfo.NodeId] = nodeinfo
-			}
-			out <- response
-		}
-	}()
-	return out
-}
-
-type StatisticsCollector struct {
-	Store *SimpleInMemoryStore
-}
-
-func (s *StatisticsCollector) Process(in chan ParsedResponse) chan ParsedResponse {
-	out := make(chan ParsedResponse)
-	go func() {
-		for response := range in {
-			if response.Type() == "statistics" {
-				statistics := response.ParsedData().(StatisticsStruct)
-				s.Store.statistics[statistics.NodeId] = statistics
-			}
-			out <- response
-		}
-	}()
-	return out
-}
-
-type NeighbourInfoCollector struct {
-	Store *SimpleInMemoryStore
-}
-
-func (n *NeighbourInfoCollector) Process(in chan ParsedResponse) chan ParsedResponse {
-	out := make(chan ParsedResponse)
-	go func() {
-		for response := range in {
-			if response.Type() == "neighbours" {
-				neighbours := response.ParsedData().(NeighbourStruct)
-				n.Store.neighbourInfos[neighbours.NodeId] = neighbours
-			}
-			out <- response
-		}
-	}()
-	return out
 }
