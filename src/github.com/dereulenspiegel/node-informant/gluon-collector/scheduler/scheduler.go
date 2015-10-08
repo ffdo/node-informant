42aed3e652dc90e7bce8592c4083fb3dc06a065f
Till Klocke <till.klocke@gmail.com>
2015-10-08 20:43:29 +0200

    Added documentation for the scheduler package. From my last commits you can guess that I am in a german train with bad internet, can you?

--- src/github.com/dereulenspiegel/node-informant/gluon-collector/scheduler/scheduler.go
+++ src/github.com/dereulenspiegel/node-informant/gluon-collector/scheduler/scheduler.go
@@ -4,12 +4,17 @@ import (
 	"time"
 )
 
+// ScheduledJob holds the state of a scheduled job like the method to execute
+// and the Ticker.
 type ScheduledJob struct {
 	ticker   *time.Ticker
 	quitChan chan bool
 	method   func()
 }
 
+// NewJob creates an new ScheduledJob which executes the given method in the
+// specified interval. The bool value fireNow determines if the method should
+// executed directly or after the specified duration has passed the first time.
 func NewJob(interval time.Duration, job func(), fireNow bool) *ScheduledJob {
 	if fireNow {
 		go job()
@@ -25,6 +30,8 @@ func NewJob(interval time.Duration, job func(), fireNow bool) *ScheduledJob {
 	return sJob
 }
 
+// Stop stops a scheduled job immediately. But it will not cancel the
+// specified method if it is long running job.
 func (s *ScheduledJob) Stop() {
 	s.quitChan <- true
 }
