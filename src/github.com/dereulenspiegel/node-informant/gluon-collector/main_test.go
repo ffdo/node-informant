8e24387683ff3ab507637979175455ca6381b70b
Till Klocke <till.klocke@gmail.com>
2015-10-12 08:41:02 +0200

    Removed unnecessary code

--- src/github.com/dereulenspiegel/node-informant/gluon-collector/main_test.go
+++ src/github.com/dereulenspiegel/node-informant/gluon-collector/main_test.go
@@ -39,8 +39,6 @@ func executeCompletePipe(t *testing.T, store data.Nodeinfostore) {
 		time.Sleep(time.Millisecond * 1)
 	}
 
-	/*time.Sleep(time.Second * 3)*/
-
 	for _, closable := range closeables {
 		closable.Close()
 	}
