a9e00632ee654febb6d42e8da2e679ef800e2c33
Till Klocke <till.klocke@gmail.com>
2015-12-21 15:44:33 +0100

    Readded test for node and graph generation in main_test.go

--- src/github.com/dereulenspiegel/node-informant/gluon-collector/main_test.go
+++ src/github.com/dereulenspiegel/node-informant/gluon-collector/main_test.go
@@ -5,13 +5,25 @@ import (
 	"testing"
 
 	"github.com/dereulenspiegel/node-informant/gluon-collector/data"
+	"github.com/dereulenspiegel/node-informant/gluon-collector/meshviewer"
 	"github.com/dereulenspiegel/node-informant/gluon-collector/test"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCompletePipe(t *testing.T) {
+	assert := assert.New(t)
 	store := data.NewSimpleInMemoryStore()
 	test.ExecuteCompletePipe(t, store)
+
+	graphGenerator := &meshviewer.GraphGenerator{Store: store}
+	nodesGenerator := &meshviewer.NodesJsonGenerator{Store: store}
+	graph := graphGenerator.GenerateGraph()
+	assert.NotNil(graph)
+	assert.Equal(232, len(graph.Batadv.Nodes))
+	assert.Equal(72, len(graph.Batadv.Links))
+
+	nodes := nodesGenerator.GetNodesJson()
+	assert.NotNil(nodes)
 }
 
 func TestCompletePipeWithBoltStore(t *testing.T) {
