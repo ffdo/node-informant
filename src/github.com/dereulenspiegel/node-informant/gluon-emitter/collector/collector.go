9dd6e18f2230d0ce8bc7d16e3f5f756d4146823c
Till Klocke <till.klocke@gmail.com>
2015-12-26 13:56:08 +0100

    Renamed StatCollector to FactCollector

--- src/github.com/dereulenspiegel/node-informant/gluon-emitter/collector/collector.go
+++ src/github.com/dereulenspiegel/node-informant/gluon-emitter/collector/collector.go
@@ -7,44 +7,44 @@ import (
 	"github.com/dereulenspiegel/node-informant/gluon-collector/scheduler"
 )
 
-type StatCollector interface {
+type FactCollector interface {
 	Init(map[string]interface{}) error
 	Collect() interface{}
 	Path() string
 }
 
-type CreateStatCollector func() StatCollector
+type CreateFactCollector func() FactCollector
 
 var (
-	statCollectorCreators []CreateStatCollector
-	statCollectors        []StatCollector
+	FactCollectorCreators []CreateFactCollector
+	FactCollectors        []FactCollector
 
 	collectJob *scheduler.ScheduledJob
 )
 
 func init() {
-	statCollectorCreators = make([]CreateStatCollector, 0, 20)
-	statCollectors = make([]StatCollector, 0, 20)
+	FactCollectorCreators = make([]CreateFactCollector, 0, 20)
+	FactCollectors = make([]FactCollector, 0, 20)
 }
 
-func Register(createColletorFunc CreateStatCollector) {
-	statCollectorCreators = append(statCollectorCreators, createColletorFunc)
+func Register(createColletorFunc CreateFactCollector) {
+	FactCollectorCreators = append(FactCollectorCreators, createColletorFunc)
 }
 
 func InitCollection(collectorConfig map[string]interface{}) {
-	for _, creator := range statCollectorCreators {
-		statCollector := creator(collectorConfig)
-		if err := statCollector.Init(); err != nil {
-			statCollectors = append(statCollectors, statCollector)
+	for _, creator := range FactCollectorCreators {
+		FactCollector := creator(collectorConfig)
+		if err := FactCollector.Init(); err != nil {
+			FactCollectors = append(FactCollectors, FactCollector)
 		} else {
 			log.WithFields(log.Fields{
 				"error":  err,
-				"metric": statCollector.Path(),
+				"metric": FactCollector.Path(),
 			}).Fatalf("Failed to initialize statistic collector")
 		}
 	}
 	collectJob = scheduler.NewJob(time.Minute*1, func() {
-		for _, collector := range statCollectors {
+		for _, collector := range FactCollectors {
 			collector.Collect()
 		}
 	}, true)
